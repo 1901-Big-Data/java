5319315faa932978bafa22a631e4cbe48dd74844
Quintin Donnelly <quinndonnelly22@gmail.com>
2019-01-31 10:18:28 -0500

    Add example of JUnit
    JUnit test can be found in the test dir of the Calculator example

---
+++ examples/Calculator/src/main/java/com/example/calculator/SimpleCalculator.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+
+public interface SimpleCalculator {
+	int add(int i, int j);
+	double add(double i, double j);
+	int subtract(int i, int j);
+	double subtract(double i, double j);
+}
