843c4b6ea5dfae62bfc852873c6140add1d765cf
Quintin Donnelly <quinndonnelly22@gmail.com>
2019-01-31 10:49:17 -0500

    Add the logging and reports
    Jacoco for reports and Log4j2 for logging

--- examples/Calculator/src/main/java/com/example/calculator/Ti84.java
+++ examples/Calculator/src/main/java/com/example/calculator/Ti84.java
@@ -2,10 +2,16 @@ package com.example.calculator;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Ti84 implements SimpleCalculator {
+	
+	private static Logger logger = LogManager.getLogger(Ti84.class);
 
 	@Override
 	public int add(int i, int j) {
+		logger.info("This is a sample log");
 		return i + j;
 	}
 
@@ -25,7 +31,7 @@ public class Ti84 implements SimpleCalculator {
 	}
 	
 	public void errorProne() throws IOException {
-		throw new IllegalAccessError();
+		throw new IOException();
 	}
 
 }
